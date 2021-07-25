c64b91165ce187126b67ce6e947bbcc468298d3c
viduni94 <viduni.ushanka@gmail.com>
2021-07-25 19:35:40 +0530

    Completed product recommendations graphs

--- src/pages/ProductRecommendations/data.js
+++ src/pages/ProductRecommendations/data.js
@@ -141,3 +141,810 @@ export const graphThreeData = [
     total_no_of_reviews: 41753,
   },
 ];
+
+// Name is product ID
+export const graphFourData = [
+  {
+    name: '2734888454',
+    avg_rating: '3.5',
+  },
+  {
+    name: '2841233731',
+    avg_rating: '5.0',
+  },
+  {
+    name: '6641040',
+    avg_rating: '4.351351351351352',
+  },
+  {
+    name: '7310172001',
+    avg_rating: '4.7514450867052025',
+  },
+  {
+    name: '7800648702',
+    avg_rating: '4.0',
+  },
+  {
+    name: '9376674501',
+    avg_rating: '5.0',
+  },
+  {
+    name: 'B00002Z754',
+    avg_rating: '5.0',
+  },
+  {
+    name: 'B00004RAMS',
+    avg_rating: '3.6333333333333333',
+  },
+  {
+    name: 'B00004RAMY',
+    avg_rating: '4.104651162790698',
+  },
+  {
+    name: 'B00004RBDZ',
+    avg_rating: '3.6271186440677967',
+  },
+  {
+    name: 'B00004RYGX',
+    avg_rating: '4.4867724867724865',
+  },
+  {
+    name: 'B00004S1C5',
+    avg_rating: '4.0',
+  },
+  {
+    name: 'B000052Y74',
+    avg_rating: '3.6',
+  },
+  {
+    name: 'B0000535LF',
+    avg_rating: '5.0',
+  },
+  {
+    name: 'B0000537KC',
+    avg_rating: '4.25',
+  },
+  {
+    name: 'B00005C2JE',
+    avg_rating: '5.0',
+  },
+  {
+    name: 'B00005C2M2',
+    avg_rating: '4.48',
+  },
+  {
+    name: 'B00005IX97',
+    avg_rating: '3.75',
+  },
+  {
+    name: 'B00005IX98',
+    avg_rating: '3.75',
+  },
+  {
+    name: 'B00005OMWO',
+    avg_rating: '4.75',
+  },
+  {
+    name: 'B00005U2FA',
+    avg_rating: '4.450704225352113',
+  },
+  {
+    name: 'B0000643LM',
+    avg_rating: '5.0',
+  },
+  {
+    name: 'B00006FMLY',
+    avg_rating: '4.333333333333333',
+  },
+  {
+    name: 'B00006G930',
+    avg_rating: '2.875',
+  },
+  {
+    name: 'B00006IDJO',
+    avg_rating: '1.0',
+  },
+  {
+    name: 'B00006IDJU',
+    avg_rating: '4.0',
+  },
+  {
+    name: 'B00006IDK9',
+    avg_rating: '4.2',
+  },
+  {
+    name: 'B00006L2ZT',
+    avg_rating: '5.0',
+  },
+  {
+    name: 'B00006LL38',
+    avg_rating: '3.909090909090909',
+  },
+  {
+    name: 'B000084346',
+    avg_rating: '5.0',
+  },
+  {
+    name: 'B00008434E',
+    avg_rating: '5.0',
+  },
+  {
+    name: 'B000084DVR',
+    avg_rating: '5.0',
+  },
+  {
+    name: 'B000084DWM',
+    avg_rating: '3.6',
+  },
+  {
+    name: 'B000084DYL',
+    avg_rating: '3.5',
+  },
+  {
+    name: 'B000084E1U',
+    avg_rating: '5.0',
+  },
+  {
+    name: 'B000084E6V',
+    avg_rating: '4.107344632768362',
+  },
+  {
+    name: 'B000084E76',
+    avg_rating: '3.5555555555555554',
+  },
+  {
+    name: 'B000084E7R',
+    avg_rating: '5.0',
+  },
+  {
+    name: 'B000084EAD',
+    avg_rating: '5.0',
+  },
+  {
+    name: 'B000084EBE',
+    avg_rating: '5.0',
+  },
+  {
+    name: 'B000084ED0',
+    avg_rating: '5.0',
+  },
+  {
+    name: 'B000084EJR',
+    avg_rating: '4.0',
+  },
+  {
+    name: 'B000084EK4',
+    avg_rating: '3.0',
+  },
+  {
+    name: 'B000084EK5',
+    avg_rating: '5.0',
+  },
+  {
+    name: 'B000084EK7',
+    avg_rating: '4.0',
+  },
+  {
+    name: 'B000084EKA',
+    avg_rating: '4.0',
+  },
+  {
+    name: 'B000084EKB',
+    avg_rating: '4.0',
+  },
+  {
+    name: 'B000084EKC',
+    avg_rating: '5.0',
+  },
+  {
+    name: 'B000084EKD',
+    avg_rating: '2.0',
+  },
+  {
+    name: 'B000084EKO',
+    avg_rating: '3.0',
+  },
+  {
+    name: 'B000084EKW',
+    avg_rating: '4.333333333333333',
+  },
+  {
+    name: '141278509X',
+    avg_rating: '5.0',
+  },
+  {
+    name: '7310172101',
+    avg_rating: '4.7514450867052025',
+  },
+  {
+    name: 'B00002N8SM',
+    avg_rating: '1.7894736842105263',
+  },
+  {
+    name: 'B00002NCJC',
+    avg_rating: '4.5',
+  },
+  {
+    name: 'B00004CI84',
+    avg_rating: '4.4867724867724865',
+  },
+  {
+    name: 'B00004CXX9',
+    avg_rating: '4.4051282051282055',
+  },
+  {
+    name: 'B00004RAMV',
+    avg_rating: '2.111111111111111',
+  },
+  {
+    name: 'B00004RAMX',
+    avg_rating: '3.64',
+  },
+  {
+    name: 'B00004RBDU',
+    avg_rating: '3.8421052631578947',
+  },
+  {
+    name: 'B00004RBDW',
+    avg_rating: '4.0',
+  },
+  {
+    name: 'B00004S1C6',
+    avg_rating: '4.590163934426229',
+  },
+  {
+    name: 'B00005344V',
+    avg_rating: '4.882352941176471',
+  },
+  {
+    name: 'B00005C2M3',
+    avg_rating: '3.2857142857142856',
+  },
+  {
+    name: 'B00005IX96',
+    avg_rating: '3.75',
+  },
+  {
+    name: 'B00005OMWQ',
+    avg_rating: '4.375',
+  },
+  {
+    name: 'B00005V3DC',
+    avg_rating: '5.0',
+  },
+  {
+    name: 'B0000691JF',
+    avg_rating: '4.333333333333333',
+  },
+  {
+    name: 'B000069KQW',
+    avg_rating: '5.0',
+  },
+  {
+    name: 'B00008433V',
+    avg_rating: '3.8333333333333335',
+  },
+  {
+    name: 'B00008434F',
+    avg_rating: '3.0',
+  },
+  {
+    name: 'B00008437P',
+    avg_rating: '3.0',
+  },
+  {
+    name: 'B000084388',
+    avg_rating: '4.75',
+  },
+  {
+    name: 'B000084DXS',
+    avg_rating: '5.0',
+  },
+  {
+    name: 'B000084E66',
+    avg_rating: '3.7142857142857144',
+  },
+  {
+    name: 'B000084E6Y',
+    avg_rating: '4.0',
+  },
+  {
+    name: 'B000084E9J',
+    avg_rating: '3.0',
+  },
+  {
+    name: 'B000084E9M',
+    avg_rating: '5.0',
+  },
+  {
+    name: 'B000084EA6',
+    avg_rating: '5.0',
+  },
+  {
+    name: 'B000084EBL',
+    avg_rating: '5.0',
+  },
+  {
+    name: 'B000084EBO',
+    avg_rating: '5.0',
+  },
+  {
+    name: 'B000084EHL',
+    avg_rating: '4.2',
+  },
+  {
+    name: 'B000084EHT',
+    avg_rating: '5.0',
+  },
+  {
+    name: 'B000084EHV',
+    avg_rating: '4.333333333333333',
+  },
+  {
+    name: 'B000084EI0',
+    avg_rating: '5.0',
+  },
+  {
+    name: 'B000084EIQ',
+    avg_rating: '3.6666666666666665',
+  },
+  {
+    name: 'B000084EIZ',
+    avg_rating: '5.0',
+  },
+  {
+    name: 'B000084EJO',
+    avg_rating: '4.0',
+  },
+  {
+    name: 'B000084EJP',
+    avg_rating: '5.0',
+  },
+  {
+    name: 'B000084EJQ',
+    avg_rating: '4.0',
+  },
+  {
+    name: 'B000084EJS',
+    avg_rating: '4.0',
+  },
+  {
+    name: 'B000084EJT',
+    avg_rating: '4.0',
+  },
+  {
+    name: 'B000084EJY',
+    avg_rating: '4.5',
+  },
+  {
+    name: 'B000084EK6',
+    avg_rating: '5.0',
+  },
+  {
+    name: 'B000084EK8',
+    avg_rating: '3.0',
+  },
+  {
+    name: 'B000084EK9',
+    avg_rating: '3.6666666666666665',
+  },
+  {
+    name: 'B000084EKG',
+    avg_rating: '5.0',
+  },
+  {
+    name: 'B000084EKL',
+    avg_rating: '5.0',
+  },
+  {
+    name: 'B000084EKM',
+    avg_rating: '4.0',
+  },
+  {
+    name: 'B000084EKQ',
+    avg_rating: '3.0',
+  },
+];
+
+export const graphFiveData = [
+  {
+    count_of_helpful_tags: 99,
+    length_of_review: 98
+  },
+  {
+    count_of_helpful_tags: 99,
+    length_of_review: 761
+  },
+  {
+    count_of_helpful_tags: 97,
+    length_of_review: 808
+  },
+  {
+    count_of_helpful_tags: 97,
+    length_of_review: 796
+  },
+  {
+    count_of_helpful_tags: 97,
+    length_of_review: 770
+  },
+  {
+    count_of_helpful_tags: 97,
+    length_of_review: 611
+  },
+  {
+    count_of_helpful_tags: 97,
+    length_of_review: 970
+  },
+  {
+    count_of_helpful_tags: 97,
+    length_of_review: 4363
+  },
+  {
+    count_of_helpful_tags: 97,
+    length_of_review: 1357
+  },
+  {
+    count_of_helpful_tags: 97,
+    length_of_review: 456
+  },
+  {
+    count_of_helpful_tags: 97,
+    length_of_review: 4363
+  },
+  {
+    count_of_helpful_tags: 96,
+    length_of_review: 483
+  },
+  {
+    count_of_helpful_tags: 96,
+    length_of_review: 1071
+  },
+  {
+    count_of_helpful_tags: 96,
+    length_of_review: 700
+  },
+  {
+    count_of_helpful_tags: 96,
+    length_of_review: 207
+  },
+  {
+    count_of_helpful_tags: 96,
+    length_of_review: 700
+  },
+  {
+    count_of_helpful_tags: 96,
+    length_of_review: 1809
+  },
+  {
+    count_of_helpful_tags: 96,
+    length_of_review: 700
+  },
+  {
+    count_of_helpful_tags: 96,
+    length_of_review: 1572
+  },
+  {
+    count_of_helpful_tags: 96,
+    length_of_review: 1308
+  },
+  {
+    count_of_helpful_tags: 96,
+    length_of_review: 1729
+  },
+  {
+    count_of_helpful_tags: 96,
+    length_of_review: 225
+  },
+  {
+    count_of_helpful_tags: 96,
+    length_of_review: 977
+  },
+  {
+    count_of_helpful_tags: 95,
+    length_of_review: 1091
+  },
+  {
+    count_of_helpful_tags: 95,
+    length_of_review: 603
+  },
+  {
+    count_of_helpful_tags: 95,
+    length_of_review: 815
+  },
+  {
+    count_of_helpful_tags: 95,
+    length_of_review: 144
+  },
+  {
+    count_of_helpful_tags: 95,
+    length_of_review: 603
+  },
+  {
+    count_of_helpful_tags: 94,
+    length_of_review: 981
+  },
+  {
+    count_of_helpful_tags: 94,
+    length_of_review: 981
+  },
+  {
+    count_of_helpful_tags: 94,
+    length_of_review: 981
+  },
+  {
+    count_of_helpful_tags: 94,
+    length_of_review: 5142
+  },
+  {
+    count_of_helpful_tags: 94,
+    length_of_review: 981
+  },
+  {
+    count_of_helpful_tags: 94,
+    length_of_review: 981
+  },
+  {
+    count_of_helpful_tags: 94,
+    length_of_review: 981
+  },
+  {
+    count_of_helpful_tags: 94,
+    length_of_review: 981
+  },
+  {
+    count_of_helpful_tags: 94,
+    length_of_review: 981
+  },
+  {
+    count_of_helpful_tags: 94,
+    length_of_review: 981
+  },
+  {
+    count_of_helpful_tags: 94,
+    length_of_review: 1085
+  },
+  {
+    count_of_helpful_tags: 94,
+    length_of_review: 981
+  },
+  {
+    count_of_helpful_tags: 94,
+    length_of_review: 144
+  },
+  {
+    count_of_helpful_tags: 94,
+    length_of_review: 103
+  },
+  {
+    count_of_helpful_tags: 93,
+    length_of_review: 486
+  },
+  {
+    count_of_helpful_tags: 93,
+    length_of_review: 254
+  },
+  {
+    count_of_helpful_tags: 93,
+    length_of_review: 981
+  },
+  {
+    count_of_helpful_tags: 93,
+    length_of_review: 254
+  },
+  {
+    count_of_helpful_tags: 93,
+    length_of_review: 254
+  },
+  {
+    count_of_helpful_tags: 93,
+    length_of_review: 1713
+  },
+  {
+    count_of_helpful_tags: 93,
+    length_of_review: 486
+  },
+  {
+    count_of_helpful_tags: 92,
+    length_of_review: 1233
+  },
+  {
+    count_of_helpful_tags: 92,
+    length_of_review: 1233
+  },
+  {
+    count_of_helpful_tags: 92,
+    length_of_review: 1373
+  },
+  {
+    count_of_helpful_tags: 92,
+    length_of_review: 1233
+  },
+  {
+    count_of_helpful_tags: 91,
+    length_of_review: 1915
+  },
+  {
+    count_of_helpful_tags: 91,
+    length_of_review: 3108
+  },
+  {
+    count_of_helpful_tags: 91,
+    length_of_review: 3108
+  },
+  {
+    count_of_helpful_tags: 91,
+    length_of_review: 3108
+  },
+  {
+    count_of_helpful_tags: 91,
+    length_of_review: 3108
+  },
+  {
+    count_of_helpful_tags: 91,
+    length_of_review: 1219
+  },
+  {
+    count_of_helpful_tags: 91,
+    length_of_review: 1219
+  },
+  {
+    count_of_helpful_tags: 91,
+    length_of_review: 1219
+  },
+  {
+    count_of_helpful_tags: 91,
+    length_of_review: 2011
+  },
+  {
+    count_of_helpful_tags: 91,
+    length_of_review: 1219
+  },
+  {
+    count_of_helpful_tags: 91,
+    length_of_review: 3108
+  },
+  {
+    count_of_helpful_tags: 90,
+    length_of_review: 1214
+  },
+  {
+    count_of_helpful_tags: 90,
+    length_of_review: 750
+  },
+  {
+    count_of_helpful_tags: 90,
+    length_of_review: 1928
+  },
+  {
+    count_of_helpful_tags: 90,
+    length_of_review: 1443
+  },
+  {
+    count_of_helpful_tags: 90,
+    length_of_review: 3248
+  },
+  {
+    count_of_helpful_tags: 90,
+    length_of_review: 784
+  },
+  {
+    count_of_helpful_tags: 90,
+    length_of_review: 3596
+  },
+  {
+    count_of_helpful_tags: 90,
+    length_of_review: 3248
+  },
+  {
+    count_of_helpful_tags: 90,
+    length_of_review: 3248
+  },
+  {
+    count_of_helpful_tags: 9,
+    length_of_review: 2395
+  },
+  {
+    count_of_helpful_tags: 9,
+    length_of_review: 252
+  },
+  {
+    count_of_helpful_tags: 9,
+    length_of_review: 99
+  },
+  {
+    count_of_helpful_tags: 9,
+    length_of_review: 1253
+  },
+  {
+    count_of_helpful_tags: 9,
+    length_of_review: 373
+  },
+  {
+    count_of_helpful_tags: 9,
+    length_of_review: 140
+  },
+  {
+    count_of_helpful_tags: 9,
+    length_of_review: 187
+  },
+  {
+    count_of_helpful_tags: 9,
+    length_of_review: 297
+  },
+  {
+    count_of_helpful_tags: 9,
+    length_of_review: 234
+  },
+  {
+    count_of_helpful_tags: 9,
+    length_of_review: 1195
+  },
+  {
+    count_of_helpful_tags: 9,
+    length_of_review: 731
+  },
+  {
+    count_of_helpful_tags: 9,
+    length_of_review: 557
+  },
+  {
+    count_of_helpful_tags: 9,
+    length_of_review: 106
+  },
+  {
+    count_of_helpful_tags: 9,
+    length_of_review: 403
+  },
+  {
+    count_of_helpful_tags: 9,
+    length_of_review: 324
+  },
+  {
+    count_of_helpful_tags: 9,
+    length_of_review: 549
+  },
+  {
+    count_of_helpful_tags: 9,
+    length_of_review: 258
+  },
+  {
+    count_of_helpful_tags: 9,
+    length_of_review: 1069
+  },
+  {
+    count_of_helpful_tags: 9,
+    length_of_review: 258
+  },
+  {
+    count_of_helpful_tags: 9,
+    length_of_review: 501
+  },
+  {
+    count_of_helpful_tags: 9,
+    length_of_review: 142
+  },
+  {
+    count_of_helpful_tags: 9,
+    length_of_review: 453
+  },
+  {
+    count_of_helpful_tags: 9,
+    length_of_review: 670
+  },
+  {
+    count_of_helpful_tags: 9,
+    length_of_review: 566
+  },
+  {
+    count_of_helpful_tags: 9,
+    length_of_review: 746
+  },
+  {
+    count_of_helpful_tags: 9,
+    length_of_review: 545
+  },
+  {
+    count_of_helpful_tags: 9,
+    length_of_review: 467
+  }
+];
